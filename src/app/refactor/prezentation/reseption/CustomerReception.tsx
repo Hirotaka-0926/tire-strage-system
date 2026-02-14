f6a2ef077c3b88a399d5087c62fb7a0d9eeee4cf
Hirotaka-0926 <abehirotaka94@gmail.com>
2026-02-15 00:51:29 +0900

    refactor tire exchange

--- src/app/refactor/prezentation/reseption/CustomerReception.tsx
+++ src/app/refactor/prezentation/reseption/CustomerReception.tsx
@@ -21,9 +21,9 @@ import CreateCustomerDialog from "@/app/refactor/prezentation/reseption/componen
 import CustomerTable from "@/app/refactor/prezentation/reseption/components/CustomerTable";
 import CustomerDetailDialog from "@/app/refactor/prezentation/reseption/components/CustomerDetailDialog";
 import EditCustomerDialog from "@/app/refactor/prezentation/reseption/components/EditCustomerDialog";
+import TireExchangeDialog from "@/app/refactor/prezentation/reseption/components/TireExchangeDialog";
 import Pagination from "@/app/refactor/prezentation/reseption/components/Pagination";
 import CustomerStats from "@/app/refactor/prezentation/reseption/components/CustomerStats";
-import TireExchangeDialog from "@/app/customer/components/TireExchangeDialog";
 
 interface Props {
   initialCustomers: Client[];
